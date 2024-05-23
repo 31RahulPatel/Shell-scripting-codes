6331a8a53baf3cc8e1373e54bcca7c3e077ddc53
31RahulPatel <patelrahul3105@gmail.com>
2024-05-23 12:15:51 +0530

    Arthimatic srcipt

---
+++ Arthimaticoperation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Enter the values: "
+echo "ENTER a VALUE: "
+read a
+echo "ENTER b VALUE: "
+read b
+
+echo "a+b= $(($a+$b))"
+echo "a-b = $(($a+$b))"
+echo "a*b= $(($a*$b))"
+echo "a/b= $(($a/$b))"
+
+
+echo "completed"
--- loops/aforloop.sh
+++ loops/aforloop.sh
@@ -4,3 +4,5 @@ for i in 1 2 3 4 5 6 8
 do
 echo $i
 done
+
+#output = 1 2 3 4 5 6 8
